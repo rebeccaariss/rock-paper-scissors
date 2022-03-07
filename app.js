93076ed4bfddb2a5f74f5e06695d2dc68cc58591
Rebecca Ariss <rebeccamarionruth@gmail.com>
2022-03-07 16:53:48 -0500

    Style buttons for user choices

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 
-const possibleChoices = document.querySelectorAll('button');
+const possibleChoices = document.querySelectorAll('input');
 
 let userChoice;
 let computerChoice;
